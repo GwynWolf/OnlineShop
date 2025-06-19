f0d21b989db472b173135bea3cd23522a1845c1f
Vlad <chvs1312@gmail.com>
2025-06-19 12:17:21 +0300

    feat: add new method in Images

--- src/main/java/com/onlineshop/dao/images/ImagesDAO.java
+++ src/main/java/com/onlineshop/dao/images/ImagesDAO.java
@@ -5,5 +5,8 @@ import com.onlineshop.entity.Images;
 import java.util.List;
 
 public interface ImagesDAO {
+    public List<Images> getAllImages();
     public List<Images> getImagesByProductID(int productID);
+
+    public void save(Images image);
 }
